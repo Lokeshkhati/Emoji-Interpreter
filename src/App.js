9c5b9b13d3a23b4d9b15152e1f19caa6aff4ed9b
Lokesh Khati <62974339+Lokeshkhati@users.noreply.github.com>
2021-12-06 02:40:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import React from "react";
+import { useState } from "react";
 import "./styles.css";
 
+const emojiDictionary = {
+  "🍇": "Grapes",
+  "🍋": "Lemon",
+  "🥺": "Red Apple",
+  "🍑": "Peach",
+  "🍓": "Strawberry",
+  "🫐": "Blueberries",
+  "🍈 ": "Melon",
+  "🍊": "Tangerine"
+};
+
+const emojiWeKnow = Object.keys(emojiDictionary);
+
 export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning =
+        "We don't have this in our database. we will update this soon :)";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <h1>inside out</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1> Fruit Emoji</h1>
+      <p>Interpreter</p>
+      <input
+        placeholder="Enter emoji here"
+        onChange={emojiInputHandler}
+      ></input>
+
+      <h2>{meaning} </h2>
+
+      {emojiWeKnow.map(function (emoji) {
+        return (
+          <span onClick={() => emojiClickHandler(emoji)} key={emoji}>
+            {" "}
+            {emoji}{" "}
+          </span>
+        );
+      })}
     </div>
   );
 }
